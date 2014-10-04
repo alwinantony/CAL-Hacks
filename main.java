afa316f3a4c2b11bef912782ac764d42a057bfb2
IPajarin <ipajarin@ucmerced.edu>
2014-10-04 02:21:01 -0700

    Create main.java

---
+++ main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class main(
+  public static void main(String[] args){
+    Scanner input = new Scanner(System.in);
+    System.out.println("Input number of chores: ");
+    Chores(input.nextInt());
+    System.out.println("Input number of people: ");
+    People(input.nextInt());
+  }
+}
