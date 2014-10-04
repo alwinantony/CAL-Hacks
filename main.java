631854bf651dcf533dcb9b69b9fb8379e7c932eb
IPajarin <ipajarin@ucmerced.edu>
2014-10-04 02:37:53 -0700

    Update main.java

--- main.java
+++ main.java
@@ -9,5 +9,12 @@ public class main(
     int peoplenum = input.nextInt();
     People(peoplenum);
     
+    System.out.println("Enter names of chores: ");
+    for (int i = 0; i < choresnum; i++)
+      addchore(i, input.nextString());
+    
+    System.out.println("Enter names of people: ");
+    for (int j = 0; j < peoplenum; j++)
+      addppl(j, input.nextString());
   }
 }
