ab9189380fcb4f094b1e97239cc22d7dfcc63bc4
alwinantony <alwinantony4@yahoo.com>
2014-10-04 03:24:48 -0700

    Update function.java

--- function.java
+++ function.java
@@ -1,6 +1,9 @@
-import java.util.Scanner;
+import java.util.*;
 public class function{
   public static void main(String[] args){
+    String cName;
+    int dNum;
+    Hashtable <String, Integer> myChores = new Hashtable <String, Integer>();
     Scanner input = new Scanner(System.in);
     System.out.println("Input number of chores: ");
     int choresnum = input.nextInt();
@@ -9,10 +12,15 @@ public class function{
     int peoplenum = input.nextInt();
     People people = new People(peoplenum);
     
-    System.out.println("Enter names of chores: ");
-    for (int i = 0; i < choresnum; i++)
-      chores.addchore(i, input.nextLine());
-    
+    System.out.println("Enter names and difficulty of chores: ");
+    for (int i = 0; i < choresnum; i++){
+      System.out.println("Input chore name: ");
+      cName = input.nextLine();
+      System.out.println("Input chore difficulty: ");
+      dNum = input.nextLine();
+      myChores.put(cName, dNum);
+    }
+     
     System.out.println("Enter names of people: ");
     for (int j = 0; j < peoplenum; j++)
       people.addppl(j, input.nextLine());
