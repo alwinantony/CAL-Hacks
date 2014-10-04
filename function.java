5b52892462eee28b4a1f9c76e66ca2f5245ad4c1
IPajarin <ipajarin@ucmerced.edu>
2014-10-04 13:19:43 -0700

    Update function.java

--- function.java
+++ function.java
@@ -1,28 +1,26 @@
 import java.util.*;
 public class function{
-  public static void main(String[] args){
-    String cName;
-    int dNum;
-    Hashtable <String, Integer> myChores = new Hashtable <String, Integer>();
-    Scanner input = new Scanner(System.in);
-    System.out.println("Input number of chores: ");
-    int choresnum = input.nextInt();
-    Chores chores = new Chores(choresnum);
-    System.out.println("Input number of people: ");
-    int peoplenum = input.nextInt();
-    People people = new People(peoplenum);
-    
-    System.out.println("Enter names and difficulty of chores: ");
-    for (int i = 0; i < choresnum; i++){
-      System.out.println("Input chore name: ");
-      cName = input.nextLine();
-      System.out.println("Input chore difficulty: ");
-      dNum = input.nextInt();
-      myChores.put(cName, dNum);
-    }
-     
-    System.out.println("Enter names of people: ");
-    for (int j = 0; j < peoplenum; j++)
-      people.addppl(j, input.nextLine());
-  }
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Input number of chores: ");
+		int chorenum = input.nextInt();
+		String[] chores = new String[chorenum];
+		Integer[] chorediff = new Integer[chorenum];
+		for (int i = 0; i < chorenum; i++) {
+			System.out.println("Input a chore name: ");
+			chores[i] = input.next();
+			System.out.println("Input \"" + chores[i] + "\" difficulty (1 being easy and 10 being hard): ");
+			chorediff[i] = input.nextInt();
+		}
+
+		System.out.println("Input number of people: ");
+		String[] people = new String[input.nextInt()];
+
+		System.out.println("Enter names of people: ");
+		for (int j = 0; j < people.length; j++)
+			people[j] = input.next();
+	
+		
+	}
 }
